7dab45ff62149c7b5ff4cd73a2f8b68783de85c1
jgarc594 <jgarc594@ucr.edu>
2023-08-06 17:41:23 -0700

    Added sports-data and news-data api's

--- CS179-Serverside/SelectSports/src/app/homepage/homepage.component.ts
+++ CS179-Serverside/SelectSports/src/app/homepage/homepage.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsDataService } from '../news-data.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  news: any[] = [];
 
+  constructor(private newsDataService: NewsDataService) { }
+
+  ngOnInit(): void {
+    this.fetchLatestAmericanSportsNews();
+  }
+
+  fetchLatestAmericanSportsNews() {
+    this.newsDataService.getLatestAmericanSportsNews().subscribe(
+      (data: any) => {
+        this.news = data.articles;
+      },
+      (error) => {
+        console.log('Error fetching American sports news', error);
+      }
+    );
+  }
 }
