8cb2f2f8a5229b481d51e38bfd3fc8389b66a0c6
Nabanes06 <91462080+Nabanes06@users.noreply.github.com>
2023-08-11 11:04:26 -0700

    Working password hiding

--- CS179-Serverside/SelectSports/src/app/login-user/login-user.component.ts
+++ CS179-Serverside/SelectSports/src/app/login-user/login-user.component.ts
@@ -1,46 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { AuthService } from '../Services/auth.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
-  styleUrls: ['./login-user.component.css']
+  styleUrls: ['./login-user.component.css'],
 })
 export class LoginUserComponent implements OnInit {
   userForm = { email: '', password: '' };
-  
-constructor(private formBuilder:FormBuilder, private auth:AuthService, private router:Router){
+  showPassword = false;
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  Login() {
+    this.auth.login(this.userForm).subscribe(
+      (res: any) => {
+        if (res.success) {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/profile']);
+        } else {
+          alert(res.message);
+        }
+      },
+      (err: any) => {
+        alert("Login Unsuccessful");
+      }
+    );
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
-
-ngOnInit(): void {
-
-}
-Login(){
-  
-  this.auth.login(this.userForm).subscribe(res => {
-    if(res.success){
-      localStorage.setItem('token',res.token);
-     this.router.navigate(['/profile']);
-      
-    }else{
-      alert(res.message)
-    }
-  }, err=>{
-    alert("Login Unsuccessful");
-  })
-  
-
-
-}
-
-
-
-
-}
-
-
-
--- CS179-Serverside/routes/Auth.js
+++ CS179-Serverside/routes/Auth.js
@@ -1,102 +1,86 @@
 const router = require('express').Router();
 const UserData = require("../models/UserData");
 const bcrypt = require('bcrypt');
-const { response } = require('express');
 const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
 const secretkey = "SportsSelectSecret";
 const VerifyAuth = require('../middleware/VerifyAuth');
 
-router.post('/register', (req,res)=>{
-    bcrypt.hash(req.body.password,10, (err, hash)=>{
-        if(err){
-            return res.json({success:false, message:"Hashing Failed"});
-        }
-        else{
+router.post('/register', (req, res) => {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+            return res.json({ success: false, message: "Hashing Failed" });
+        } else {
             const user = new UserData({
                 name: req.body.name,
                 email: req.body.email,
                 password: hash,
                 points: req.body.points,
-            })
-       
-       
+            });
+
             user.save()
-            .then(()=>{
-                res.json({success:true, message:"Account Created"})
-               
-            })
-            .catch((err)=>{
-                if(err.code === 11000){
-                    
-                    return res.json({success:false, message:"Email Already Exists!"});
-
-                }
-                res.json({success:false, message:"Account Creation Failed"});
-            })
-
-     }
-
-    });
-    
-});
-
-
-router.post('/SignIn', (req, res)=>{
-    UserData.find({email:req.body.email}).exec().then((response)=>{
-        if(response.length<1){
-            return res.json({success: false, message:"Invalid Email"});
+                .then(() => {
+                    res.json({ success: true, message: "Account Created" });
+                })
+                .catch((err) => {
+                    if (err.code === 11000) {
+                        return res.json({ success: false, message: "Email Already Exists!" });
+                    }
+                    res.json({ success: false, message: "Account Creation Failed" });
+                });
         }
-     else{
-           const user = response[0];
-           bcrypt.compare(req.body.password, user.password, (err, ret)=>{
-            if(ret){
-                const payload ={
-                    userId: user._id
-
-                }
-                const token = jwt.sign(payload, secretkey );
-                return res.json({success:true, token:token, message:"Successfully logged in"});
-            }else{
-                return res.json({success:false, message:"Invalid Password"})
-            }
-        })
-    }
-        
-
-    }).catch(err =>{
-        res.json({success: false, message:"Login Failed"});
-    })
+    });
 });
 
-router.get('/profile', VerifyAuth, (req, res)=>{
-   
-    const userId= req.userData.userId
-    //console.log(userId);
-    UserData.findById(userId).exec().then((response)=>{
-        res.json({success:true, data:response})
+router.post('/SignIn', (req, res) => {
+    UserData.findOne({ email: req.body.email }).exec()
+      .then((user) => {
+        if (!user) {
+          return res.json({ success: false, message: "Invalid Email" });
+        } else {
+          bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+            if (isMatch) {
+              const payload = {
+                userId: user._id
+              };
+              const token = jwt.sign(payload, secretkey);
+              return res.json({ success: true, token: token, message: "Successfully logged in" });
+            } else {
+              return res.json({ success: false, message: "Invalid Password" });
+            }
+          });
+        }
+      })
+      .catch(err => {
+        res.json({ success: false, message: "Login Failed" });
+      });
+  });
+  
 
-    }).catch(err=>{
-        res.json({success:false, message: "Fetching User failed"})
-    })
+router.get('/profile', VerifyAuth, (req, res) => {
+    const userId = req.userData.userId;
+    UserData.findById(userId).exec()
+        .then((response) => {
+            res.json({ success: true, data: response });
+        })
+        .catch(err => {
+            res.json({ success: false, message: "Fetching User failed" });
+        });
 });
 
 router.post('/profileEdit', (req, res) => {
-  const { email, profileID, favorite_sport, description } = req.body;
+    const { email, profileID, favorite_sport, description } = req.body;
 
-  UserData.findOneAndUpdate({ email: email }, { profileID: profileID, favorite_sport: favorite_sport, description: description }, { new: true })
-    .then(updatedUser => {
-      if (!updatedUser) {
-        return res.json({ success: false, message: "User not found" });
-      }
-      return res.json({ success: true, message: "Profile updated successfully", data: updatedUser });
-    })
-    .catch(err => {
-      console.error(err);
-      return res.json({ success: false, message: "Error updating profile" });
-    });
+    UserData.findOneAndUpdate({ email: email }, { profileID: profileID, favorite_sport: favorite_sport, description: description }, { new: true })
+        .then(updatedUser => {
+            if (!updatedUser) {
+                return res.json({ success: false, message: "User not found" });
+            }
+            return res.json({ success: true, message: "Profile updated successfully", data: updatedUser });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.json({ success: false, message: "Error updating profile" });
+        });
 });
 
 module.exports = router;
-
-
