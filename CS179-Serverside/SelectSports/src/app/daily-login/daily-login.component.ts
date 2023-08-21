7fb7178db71ce6ca5a06490f2dc31bb2a77966b6
FengchunFan <ffan005@ucr.edu>
2023-08-20 19:22:11 -0700

    slot machine ready?

--- CS179-Serverside/SelectSports/src/app/daily-login/daily-login.component.ts
+++ CS179-Serverside/SelectSports/src/app/daily-login/daily-login.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from '../Services/auth.service';
 export class DailyLoginComponent implements OnInit {
   userForm = { email: '', points: 0, dailyAccessTime: 0};
   Counter: number = 0;
-  maxC: number = 1000;
+  maxC: number = 2;
 
   constructor(private auth:AuthService, private router: Router) { }
 
@@ -21,6 +21,7 @@ export class DailyLoginComponent implements OnInit {
       this.userForm.points = currentUser.points;
       this.userForm.dailyAccessTime = Date.now();
     }
+    this.playbackground();
   }
 
   DailyLogin() {
@@ -35,6 +36,7 @@ export class DailyLoginComponent implements OnInit {
 
   addPoints() {
     if (this.Counter < this.maxC) {
+      this.adjustbgm(0.4);
       const rollingReelImage1 = document.getElementById('rolling_reel-image-1');
       const reelImage1 = document.getElementById('reel-image-1');
       const rollingReelImage2 = document.getElementById('rolling_reel-image-2');
@@ -43,12 +45,17 @@ export class DailyLoginComponent implements OnInit {
       const reelImage3 = document.getElementById('reel-image-3');
 
       if (rollingReelImage1 && reelImage1 && rollingReelImage2 && reelImage2 && rollingReelImage3 && reelImage3) {
+        const slotSound = document.getElementById('slotSound') as HTMLAudioElement;
+        slotSound.volume = 0.6;
+        slotSound.currentTime = 0;
+        slotSound.play();
         rollingReelImage1.style.display = 'inline';
         reelImage1.style.display = 'none';
         rollingReelImage2.style.display = 'inline';
         reelImage2.style.display = 'none';
         rollingReelImage3.style.display = 'inline';
         reelImage3.style.display = 'none';
+
         setTimeout(() => {
           const randomNumber1 = Math.floor(Math.random() * 9) + 1;
           const newrolling1 = document.getElementById('rolling_reel-image-1'+randomNumber1);        
@@ -71,29 +78,39 @@ export class DailyLoginComponent implements OnInit {
                 newrolling3.style.display = 'inline';
               }
               setTimeout(() => {
+                slotSound.pause();
                 var PointsToAdd = 0;
                 if(randomNumber1 == randomNumber2 && randomNumber2 == randomNumber3){
                   PointsToAdd = 7777;
+                  const endSound = document.getElementById('endSound3') as HTMLAudioElement;
+                  endSound.play();
                 }else if (randomNumber1 == randomNumber2 || randomNumber1 == randomNumber3 || randomNumber2 == randomNumber3){
                   PointsToAdd = 2666;
+                  const endSound = document.getElementById('endSound2') as HTMLAudioElement;
+                  endSound.play();
                 }else{
                   PointsToAdd = 188;
+                  const endSound = document.getElementById('endSound') as HTMLAudioElement;
+                  endSound.play();
                 }
                 this.userForm.points += PointsToAdd;
                 this.Counter++;
-                alert("Congratulations!! You got " + PointsToAdd + " points");
-                if(newrolling1 && newrolling2 && newrolling3){
-                  newrolling1.style.display = 'none';
-                  reelImage1.style.display = 'inline';
-                  newrolling2.style.display = 'none';
-                  reelImage2.style.display = 'inline';
-                  newrolling3.style.display = 'none';
-                  reelImage3.style.display = 'inline';
-                }
-              }, 800);
-            }, 800);
-          }, 800);    
-        }, 4500);
+                setTimeout(() => {
+                  alert("Congratulations!! You got " + PointsToAdd + " points");
+                  this.adjustbgm(1);
+                  if(newrolling1 && newrolling2 && newrolling3){
+                    newrolling1.style.display = 'none';
+                    reelImage1.style.display = 'inline';
+                    newrolling2.style.display = 'none';
+                    reelImage2.style.display = 'inline';
+                    newrolling3.style.display = 'none';
+                    reelImage3.style.display = 'inline';
+                  }
+                },400);
+              }, 600);
+            }, 600);
+          }, 600);    
+        }, 2800);
       }      
     }else {
       alert('You have reached daily points limit.');
@@ -102,6 +119,7 @@ export class DailyLoginComponent implements OnInit {
 
   cheat() {
     if (this.Counter < this.maxC) {
+      this.adjustbgm(0.4);
       const rollingReelImage1 = document.getElementById('rolling_reel-image-1');
       const reelImage1 = document.getElementById('reel-image-1');
       const rollingReelImage2 = document.getElementById('rolling_reel-image-2');
@@ -110,12 +128,17 @@ export class DailyLoginComponent implements OnInit {
       const reelImage3 = document.getElementById('reel-image-3');
 
       if (rollingReelImage1 && reelImage1 && rollingReelImage2 && reelImage2 && rollingReelImage3 && reelImage3) {
+        const slotSound = document.getElementById('slotSound') as HTMLAudioElement;
+        slotSound.volume = 0.6;
+        slotSound.currentTime = 0;
+        slotSound.play();
         rollingReelImage1.style.display = 'inline';
         reelImage1.style.display = 'none';
         rollingReelImage2.style.display = 'inline';
         reelImage2.style.display = 'none';
         rollingReelImage3.style.display = 'inline';
         reelImage3.style.display = 'none';
+
         setTimeout(() => {
           const randomNumber1 = Math.floor(Math.random() * 9) + 1;
           const newrolling1 = document.getElementById('rolling_reel-image-1'+randomNumber1);        
@@ -138,33 +161,54 @@ export class DailyLoginComponent implements OnInit {
                 newrolling3.style.display = 'inline';
               }
               setTimeout(() => {
+                slotSound.pause();
                 var PointsToAdd = 0;
                 if(randomNumber1 == randomNumber2 && randomNumber2 == randomNumber3){
                   PointsToAdd = 7777;
+                  const endSound = document.getElementById('endSound3') as HTMLAudioElement;
+                  endSound.play();
                 }else if (randomNumber1 == randomNumber2 || randomNumber1 == randomNumber3 || randomNumber2 == randomNumber3){
                   PointsToAdd = 2666;
+                  const endSound = document.getElementById('endSound2') as HTMLAudioElement;
+                  endSound.play();
                 }else{
                   PointsToAdd = 188;
+                  const endSound = document.getElementById('endSound') as HTMLAudioElement;
+                  endSound.play();
                 }
                 this.userForm.points += PointsToAdd;
                 this.Counter++;
-                alert("Congratulations!! You got " + PointsToAdd + " points");
-                if(newrolling1 && newrolling2 && newrolling3){
-                  newrolling1.style.display = 'none';
-                  reelImage1.style.display = 'inline';
-                  newrolling2.style.display = 'none';
-                  reelImage2.style.display = 'inline';
-                  newrolling3.style.display = 'none';
-                  reelImage3.style.display = 'inline';
-                }
-              }, 800);
-            }, 800);
-          }, 800);    
-        }, 4500);
+                setTimeout(() => {
+                  alert("Congratulations!! You got " + PointsToAdd + " points");
+                  this.adjustbgm(1);
+                  if(newrolling1 && newrolling2 && newrolling3){
+                    newrolling1.style.display = 'none';
+                    reelImage1.style.display = 'inline';
+                    newrolling2.style.display = 'none';
+                    reelImage2.style.display = 'inline';
+                    newrolling3.style.display = 'none';
+                    reelImage3.style.display = 'inline';
+                  }
+                },400);
+              }, 600);
+            }, 600);
+          }, 600);    
+        }, 2800);
       }      
     }else {
       alert('You have reached daily points limit.');
     }
+  }
+
+  playbackground(){
+    const bgm = document.getElementById('backgroundSound') as HTMLAudioElement;
+    bgm.currentTime = 0;
+    bgm.play();
+  }
+
+  adjustbgm(volume:number) {
+    const bgm = document.getElementById('backgroundSound') as HTMLAudioElement;
+    bgm.volume = volume;
   }
 
 }
