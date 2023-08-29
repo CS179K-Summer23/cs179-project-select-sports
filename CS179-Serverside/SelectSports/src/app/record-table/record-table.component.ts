997aa58fc86c9b55072a9f246ed45e8111df95c9
FengchunFan <ffan005@ucr.edu>
2023-08-29 14:30:54 -0700

    finished record table

--- CS179-Serverside/SelectSports/src/app/record-table/record-table.component.ts
+++ CS179-Serverside/SelectSports/src/app/record-table/record-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AuthService } from '../Services/auth.service';
 import { Router } from '@angular/router';
 
@@ -8,7 +8,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./record-table.component.css']
 })
 export class RecordTableComponent implements OnInit{
-  data: any;
+  currentPage = 0;
+  itemsPerPage = 10;
+  @Input() data: { pointsinfo: string[] } = { pointsinfo: [] };
 
   constructor(private auth: AuthService, private router: Router) {}
 
@@ -18,6 +20,30 @@ export class RecordTableComponent implements OnInit{
 
   back_to_profile() {  
     this.router.navigate(['/profile']);
+  }
+
+  getDisplayedPointsInfo(): string[] {
+    const reversedPointsInfo = this.data.pointsinfo.slice().reverse();
+    const startIndex = this.currentPage * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return reversedPointsInfo.slice(startIndex, endIndex);
+  }
+
+  nextPage(): void {
+    const totalPages = Math.ceil(this.data.pointsinfo.length / this.itemsPerPage);
+    if (this.currentPage < totalPages - 1) {
+      this.currentPage++;
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.data.pointsinfo.length / this.itemsPerPage);
   }
 
   RecordTable() {
